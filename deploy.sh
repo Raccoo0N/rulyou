8e1841cbcc160279c766df9225f7810e6fd0ab79
Raccoon <133687893+Raccoo0N@users.noreply.github.com>
2024-09-20 18:58:48 +0300

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash 
-rsync -rvza --exclude '.*' --exclude '_*' --exclude '*.html' --exclude 'TODO*' --exclude '*.sh' /Users/l0cust/rsync/TOTAL/PROJECTS/FL/userapi locust@locust:/home/locust
