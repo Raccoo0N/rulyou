d00c3c121ecb1d84aa54f686ee93505dfbcab081
Raccoon <133687893+Raccoo0N@users.noreply.github.com>
2024-09-20 18:58:12 +0300

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -2,10 +2,10 @@
 	//
 	// DATABASE
 	//
-	define('DBHOST', "81.31.247.100"); 
-	define('DBUSER', "CTxMBp"); 
-	define('DBPASSWD', "SLcTgBZahrgjWNZU");
-	define('DBNAME', "PXKuRnQe"); 
+	define('DBHOST', ""); 
+	define('DBUSER', ""); 
+	define('DBPASSWD', "");
+	define('DBNAME', ""); 
 	//
 	// TABLES
 	//
